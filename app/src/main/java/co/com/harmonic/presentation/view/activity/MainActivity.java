7748b0d398d81742e0600467d864e6a98a0a4fb5
jachqaz <jach_1997@hotmail.com>
2017-12-16 10:22:46 -0500

    En AuthActivty se crea su slidenave

--- app/src/main/java/co/com/harmonic/presentation/view/activity/MainActivity.java
+++ app/src/main/java/co/com/harmonic/presentation/view/activity/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity
         GeneralFragment generalFragment = GeneralFragment.getInstance();
         replaceFragment(generalFragment, false);
     }
-
     public void replaceFragment(Fragment fragment, boolean addToBackStack) {
         FragmentManager fragmentManager = getSupportFragmentManager();
 
