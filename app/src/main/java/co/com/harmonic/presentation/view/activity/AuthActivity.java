b7f4f30ca9e214eea8bf634dcc20ce7855ebabbc
jachqaz <jach_1997@hotmail.com>
2017-12-16 10:44:32 -0500

    Se eliminan los tres puntos del AuthActivity

--- app/src/main/java/co/com/harmonic/presentation/view/activity/AuthActivity.java
+++ app/src/main/java/co/com/harmonic/presentation/view/activity/AuthActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -74,12 +73,6 @@ public class AuthActivity extends AppCompatActivity implements AuthContract.View
         finish();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
