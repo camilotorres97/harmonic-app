d05cdcc6ae59226321fb00fff3589f57e9a4eafc
Juan Camilo Torres <juankmilo9740@hotmail.com>
2017-12-17 18:17:15 -0500

    Se corrige problema al cargar las opciones del drawer layout ya que los id's no existian.

--- app/src/main/java/co/com/harmonic/presentation/view/activity/MainActivity.java
+++ app/src/main/java/co/com/harmonic/presentation/view/activity/MainActivity.java
@@ -106,17 +106,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inicio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_categories) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_configuration) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_help) {
+
+        } else if (id == R.id.nav_login) {
 
         }
 
